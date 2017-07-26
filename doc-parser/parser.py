df577009765a01fdf1f66e08bad207bc6f63eca4
马柏樟 <mabz@chanjet.com>
2017-07-26 12:00:02 +0800

    python-docx

---
+++ doc-parser/parser.py
@@ -0,0 +1,18 @@
+# encoding=utf-8
+from docx import Document
+
+PATH = 'data/T+说明书.docx'
+
+
+def read_document():
+    return Document(PATH)
+
+
+def parse_block():
+    document = read_document()
+    for i, para in enumerate(document.paragraphs):
+        print(para.text)
+
+
+if __name__ == '__main__':
+    parse_block()
